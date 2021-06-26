3fa50e59cd81585df667942c486c55a57cf48f86
Prasanth <prasanth.m@experionglobal.com>
2021-06-26 21:46:17 +0530

    Pushed changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import {trigger, style, animate, transition, state} from '@angular/animations';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      state('void', style({
+        opacity: 0
+      })),
+      transition('void <=> *', animate(1000)),
+    ])
+  ]
 })
 export class AppComponent {
-  title = 'my-todo-list';
+
+	/* An empty array that is responsible
+	to add a division */
+	public items = [];
+
+	/* A two-way binding performed which
+	pushes text on division */
+	public newTask
+  constructor(private toastr: ToastrService) {
+
+  }
+
+
+	/* When input is empty, it will
+	not create a new division */
+	public addToList() {
+		if (!this.newTask) {
+      this.toastr.error('Please enter To-Do List');
+		}
+		else {
+			this.items.push(this.newTask);
+			this.newTask = '';
+		}
+	}
+
+	/* This function takes to input the
+	task, that has to be deleted*/
+	public deleteTask(index) {
+		this.items.splice(index, 1);
+	}
 }
